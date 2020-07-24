5e6dd595f4405c2627fc1ea614c910b901a754ed
k-shizukuda <k.shizukuda@icloud.com>
2020-07-24 17:39:35 +0900

    add-address-custom-validation

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,5 +1,12 @@
 class Address < ApplicationRecord
   belongs_to :user, optional: true
 
-  validates :zip_code, :prefecture, :municipality, :address, presence: true
+  validates :zip_code,
+            presence: true,
+            #郵便番号に一致する文字列のみ可
+            format: { with: /\A\d{3}-?{4}\z/}
+  validates :prefecture, :municipality, :address, presence: true
+  validates :phone_number, allow_blank: true, #空でも可
+            #ハイフンなし10桁か11桁のみ可
+            format: { with: /\A\d{10,11}\z/}
 end
