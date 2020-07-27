027eab48aa1dde4c8aef8af723bc0f756d13e955
k-shizukuda <k.shizukuda@icloud.com>
2020-07-27 14:59:35 +0900

    create-user-login-view

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
-  root 'items#index'
+  root 'tops#index'  #rootをitemsからtopsに変更
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
