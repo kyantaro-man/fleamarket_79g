7d43619809ba9b01b4f141c69817291ba50e9144
NobuFuku <67360481+NobuFuku@users.noreply.github.com>
2020-07-27 18:23:50 +0900

    1

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-     registrations: 'users/registrations' 
+    registrations: 'users/registrations' 
   }
   devise_scope :user do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
-  root 'items#index'
-
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  
+  get root to: 'tops#index'
+  
+  post 'items',to: 'items#new'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html 
+  
 end
