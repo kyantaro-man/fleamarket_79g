6b044f655b5fb8fbaf1069e9d121600a16ea1e69
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2020-07-29 18:36:16 +0900

    resolve conflict 2nd

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
     get 'addresses', to: 'users/registrations#new_address'
     post 'addresses', to: 'users/registrations#create_address'
   end
-  root 'tops#index'
 
   resources :cards, only: [:index, :new, :create, :destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
