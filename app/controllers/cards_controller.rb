b5ca6abab9da5dd83919829e1d6323cebddd0533
kyantaro-man <67139948+kyantaro-man@users.noreply.github.com>
2020-07-29 19:41:20 +0900

    Update cards_controller.rb

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -27,7 +27,7 @@ class CardsController < ApplicationController
   end
 
   def new
-    card = Card.where(user_id: current_user.id).first
+    card = Card.find_by(user_id: current_user.id)
     redirect_to action: "index" if card.present?
   end
 
