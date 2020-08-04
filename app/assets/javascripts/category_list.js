2b4a8e6b2e27f294f8bef9435d2087f1474a46b1
k-shizukuda <k.shizukuda@icloud.com>
2020-08-04 18:14:31 +0900

    fix-category-list-js-load

--- app/assets/javascripts/category_list.js
+++ app/assets/javascripts/category_list.js
@@ -1,4 +1,4 @@
-$(function(){
+$(document).on('turbolinks:load', function() {              //初回読み込み、リロード、ページ切り替えで動く
   //子カテゴリのhtml作成用の関数
   function buildChildHTML(child){
     let html = `<li class="ChildCategory">
