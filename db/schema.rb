845e7d910d2854e64a626c1b4e4351010e9a0fb7
k-shizukuda <k.shizukuda@icloud.com>
2020-08-04 21:44:33 +0900

    fix-addressTable-zipCode-string

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_03_030646) do
+ActiveRecord::Schema.define(version: 2020_08_04_123018) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "zip_code", null: false
+    t.string "zip_code", null: false
     t.string "prefecture", null: false
     t.string "municipality", null: false
     t.string "address", null: false
