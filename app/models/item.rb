d4d80cf0ea2a95f1abd85b9104bf83eda5655a18
k-shizukuda <k.shizukuda@icloud.com>
2020-08-06 12:32:45 +0900

    add-method-models-item

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,8 +7,14 @@ class Item < ApplicationRecord
 
   belongs_to :prefecture, optional: true
   belongs_to :category, optional: true
-  
-
+  # 1つ前のitemを取得するためのインスタンスメソッド   @item.previousで呼び出せます
+  def previous
+    Item.where("id < ?", self.id).order("id DESC").first       #@itemよりidが小さいレコードを降順で並べ最初を取得
+  end
+  # 1つ後のitemを取得するためのインスタンスメソッド   @item.nextで呼び出せます
+  def next
+    Item.where("id > ?", self.id).order("id ASC").first        #@itemよりidが大きいレコードを昇順で並べ最初を取得
+  end
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to_active_hash :condition
   belongs_to_active_hash :postageplayer
