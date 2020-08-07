b999fc10d6933e5dd750cb971bb663788f89e2de
k-shizukuda <k.shizukuda@icloud.com>
2020-08-07 21:27:38 +0900

    make-item-show-comment- asynchronous-function

---
+++ app/assets/javascripts/comment.js
@@ -0,0 +1,62 @@
+$(document).on('turbolinks:load', function(){
+  // - if @comments
+  // - @comments.each do |comment|
+  //   .CommentField__comment
+  //     .CommentContent
+  //       .CommentContent__user
+  //         = comment.user.nickname
+  //         - if comment.user.id == @item.user.id
+  //           %span (出品者)
+  //       .CommentContent__text
+  //         = comment.text
+
+  function buildcommentHTML(comment,id){
+    if(comment.user_id == id.id){
+      let html = `<div class="CommentContent">
+                    <div class="CommentContent__user">
+                      ${comment.user_name}
+                      <span>出品者</span>
+                    </div>
+                    <div class="CommentContent__text">
+                      ${comment.text}
+                    </div>
+                  </div>`
+      return html;
+    }else{
+      let html = `<div class="CommentContent">
+                    <div class="CommentContent__user">
+                      ${comment.user_name}
+                    </div>
+                    <div class="CommentContent__text">
+                      ${comment.text}
+                    </div>
+                  </div>`
+      return html;
+    }
+  }
+
+
+
+  $('#new_comment').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+
+    })
+    .done(function(data){
+      let id = $(".CommentContent__user").data()
+      let html = buildcommentHTML(data, id);
+      $('.CommentField').append(html);
+      $('.CommentForm__textarea').val('');
+      $('.CommentForm__submit').prop('disabled', false);
+      $('.CommentField').animate({ scrollTop: $('.CommentField')[0].scrollHeight});
+    })
+  })
+});
