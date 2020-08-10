23d37ffce6810efaea0bb1df978b3203723f365b
Echizen-Rikiya <rkkyy0804@gmail.com>
2020-08-10 11:26:45 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :set_card
+  before_action :set_card, except: :show
   before_action :set_item, only: [:edit, :update,:show, :destroy, :buy, :purchase]
 
   def index
